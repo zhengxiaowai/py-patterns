f1ca8f0e9a6241777d2dd29bf8588d82772f3796
zhengxiaowai <h1x2y3awalm@gmail.com>
2017-04-16 23:49:28 +0800

    docs: structual patterns readme
    add code to readme

--- structural/adapter.py
+++ structural/adapter.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class Duck(object):
     """ 鸭子的动作 """
+
     def quack(self):
         print('Quack')
-    
+
     def fly(self):
         print('fly')
 
 
 class Turkey(object):
     """ 火鸡的动作 """
+
     def gobble(self):
         print('gobble')
-    
+
     def fly(self):
         print('fly a short distaance')
 
+
 class TurkeyAadpter(object):
     """ 火鸡适配器，转化成鸭子 """
+
     def __init__(self, turkey):
         self.turkey = turkey
 
     def quack(self):
         self.turkey.gobble()
-    
+
     def fly(self):
         """ 火鸡飞行距离是鸭子的五分之一 """
         for _ in range(5):
             self.turkey.fly()
 
+
 def duck_activity(duck):
     duck.quack()
     duck.fly()
+
 
 if __name__ == '__main__':
     # 鸭子正常活动
@@ -43,4 +50,3 @@ if __name__ == '__main__':
     # 某天鸭子走丢了，一只火鸡冒充了鸭子
     turkey = Turkey()
     duck_activity(TurkeyAadpter(turkey))
-
